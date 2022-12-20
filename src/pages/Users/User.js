c0a8aa22a5e869389965a01f0eeb5a50f0f129af
ngotruonganh <ngotruonganh1901@gmail.com>
2022-12-20 14:51:42 +0700

    Update

--- src/pages/Users/User.js
+++ src/pages/Users/User.js
@@ -8,7 +8,8 @@ class User extends React.Component {
   };
 
   async componentDidMount() {
-    let res = await axios.get("https://reqres.in/api/users?page=2");
+    let res = await axios.get("http://localhost:8080/api/v1/user");
+    console.log(res);
     this.setState({
       listUser: res && res.data && res.data.data ? res.data.data : [],
     });
@@ -22,6 +23,8 @@ class User extends React.Component {
           listUser.map((item, index) => {
             return (
               <div key={index}>
+                {item.email}
+                {item.password}
                 {item.id} - {item.first_name} - {item.last_name} -{" "}
                 <img src={item.avatar} alt="avatar"></img>
                 <Link to="/user/{item.id}">detail</Link>
